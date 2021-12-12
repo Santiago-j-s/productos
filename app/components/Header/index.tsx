7c712c03c56d091c05afc778f8ebcf7a66af2423
Santiago Santana <santana.santiago@gmail.com>
2021-12-12 00:53:20 -0300

    feat(header): add header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import logo from "./logo.png";
+import shoppingCart from "./cart.png";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="header">
+      <div className="header__brand">
+        <img className="header__logo" src={logo} alt="logo" />
+        <b>Ez</b>shop
+      </div>
+      <div className="header__cart">
+        <strong>$2.525,30</strong>
+        <img
+          className="header__cart-icon"
+          src={shoppingCart}
+          alt="shopping cart"
+        />
+      </div>
+    </header>
+  );
 }

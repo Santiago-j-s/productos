777c534bdfd3c98a139657b2dce8ca08e0ed8a96
Santiago Santana <santana.santiago@gmail.com>
2021-12-14 03:43:12 -0300

    feat(products): add infinite scroll, first try

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,6 @@
-import { json, Link, LoaderFunction, useLoaderData } from "remix";
+import { useEffect, useRef, useState } from "react";
+import { json, LoaderFunction, useFetcher, useLoaderData } from "remix";
+
 import type { Products } from "~/services/getProducts";
 import getProducts from "~/services/getProducts";
 import Product from "~/components/Product";
@@ -10,14 +12,66 @@ export const loader: LoaderFunction = async ({ request }) => {
   return json(products);
 };
 
+interface CurrentData {
+  page: number;
+  products: Products["products"];
+  finishedLoading: boolean;
+}
+
 export default function Index() {
   const { products, page, isLastPage } = useLoaderData<Products>();
+
+  const [currentData, setCurrentData] = useState<CurrentData>({
+    page: page,
+    products: products,
+    finishedLoading: isLastPage,
+  });
+
+  const productsContainerElement = useRef<HTMLDivElement>(null);
+  const fetcher = useFetcher<Products>();
+
+  useEffect(() => {
+    if (fetcher.type === "done") {
+      setCurrentData({
+        page: fetcher.data.page,
+        finishedLoading: fetcher.data.isLastPage,
+        products: [...currentData.products, ...fetcher.data.products],
+      });
+    }
+  }, [fetcher.type]);
+
+  useEffect(() => {
+    const callback: IntersectionObserverCallback = (entries) => {
+      for (const entry of entries) {
+        console.log(entry.boundingClientRect);
+        if (entry.isIntersecting && !currentData.finishedLoading) {
+          console.log("intersecting");
+          fetcher.load(`?page=${currentData.page + 1}`);
+          setCurrentData({ ...currentData, page: currentData.page + 1 });
+        }
+      }
+    };
+
+    const options: IntersectionObserverInit = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.7,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(productsContainerElement.current as HTMLElement);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [productsContainerElement]);
 
   return (
     <main className="main">
       <h1>Almacén</h1>
-      <div className="products-container">
-        {products.map(
+      <div ref={productsContainerElement} className="products-container">
+        {currentData.products.map(
           ({ id, photo, name, showOriginalPrice, price, originalPrice }) => (
             <Product
               key={id}
@@ -31,11 +85,6 @@ export default function Index() {
           )
         )}
       </div>
-      {isLastPage ? null : (
-        <Link className="products-container__button" to={`?page=${page + 1}`}>
-          Cargar más productos
-        </Link>
-      )}
     </main>
   );
 }

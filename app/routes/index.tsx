b48dd44140da0f8ced559c22d401ae546f59a8ce
Santiago Santana <santana.santiago@gmail.com>
2021-12-15 00:38:56 -0300

    Revert last two commits
    Cloudflare workers operate before the request hits the CDN, cache
    headers didn't work. And Cache API is only available on custom domains.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,17 +5,6 @@ import type { Products as ProductsType } from "~/services/getProducts";
 
 import Products from "~/components/Products";
 
-// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
-async function digest(message: string, algorithm = "SHA-1") {
-  const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
-  const hashBuffer = await crypto.subtle.digest(algorithm, msgUint8); // hash the message
-  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
-  const hashHex = hashArray
-    .map((b) => b.toString(16).padStart(2, "0"))
-    .join(""); // convert bytes to hex string
-  return hashHex;
-}
-
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const page = Number(url.searchParams.get("page") ?? 1);
@@ -25,8 +14,7 @@ export const loader: LoaderFunction = async ({ request }) => {
     headers: {
       "Cache-Control": `public, max-age=60, stale-while-revalidate=${
         3600 - 60
-      }, must-revalidate`,
-      Etag: await digest(JSON.stringify(products)),
+      }`,
     },
   });
 };

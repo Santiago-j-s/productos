12b6db0572bf6d399e408792e8876beeef7eb811
Santiago Santana <santana.santiago@gmail.com>
2021-12-15 10:37:27 -0300

    feat(cache): try with s-max-age

--- app/routes/images/$productId.tsx
+++ app/routes/images/$productId.tsx
@@ -8,6 +8,6 @@ export const loader: LoaderFunction = async ({ params }) => {
 
   return new Response(response.body, {
     ...response,
-    headers: { "cache-control": "public, max-age=3600" },
+    headers: { "cache-control": "public, max-age=3600, s-max-age=3600" },
   });
 };

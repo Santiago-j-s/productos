73e41bbdad14d22ac379685d3dcf66cacf2c61ba
Santiago Santana <santana.santiago@gmail.com>
2021-12-19 02:57:00 -0300

    feat: add local cart for each product

--- app/components/Product/index.tsx
+++ app/components/Product/index.tsx
@@ -1,8 +1,52 @@
-import type { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import type { Product } from "~/services/getProducts";
 
 function withExtension(photo: string, ext: string) {
   return photo.replace(/\.jpg/, `.${ext}`);
+}
+
+function useCart(): [number, () => void, () => void] {
+  const [quantity, setQuantity] = useState(() => 0);
+
+  const addToCart = () => setQuantity((q) => q + 1);
+  const removeFromCart = () => setQuantity((q) => q - 1);
+
+  return [quantity, addToCart, removeFromCart];
+}
+
+interface ButtonsProps {
+  quantity: number;
+  addToCart: () => void;
+  removeFromCart: () => void;
+}
+
+function Buttons({
+  quantity,
+  addToCart,
+  removeFromCart,
+}: ButtonsProps): ReactElement {
+  if (quantity === 0) {
+    return (
+      <button onClick={addToCart} className="product__add_to_cart_button">
+        Agregar al carrito
+      </button>
+    );
+  }
+
+  return (
+    <div className="product__buttons">
+      <button
+        className="product__update_quantity_button"
+        onClick={removeFromCart}
+      >
+        −
+      </button>{" "}
+      <span className="product__quantity">{quantity} </span>
+      <button className="product__update_quantity_button" onClick={addToCart}>
+        +
+      </button>
+    </div>
+  );
 }
 
 export default function Index({
@@ -13,6 +57,8 @@ export default function Index({
   originalPrice,
   price,
 }: Omit<Product, "priceInDollars" | "isRecent">): ReactElement {
+  const [quantity, addToCart, removeFromCart] = useCart();
+
   return (
     <div key={id} className="product">
       <picture>
@@ -32,7 +78,11 @@ export default function Index({
         ) : null}
         <span className="product__new-price">${price}</span>
       </p>
-      <button className="product__button">Agregar al carrito</button>
+      <Buttons
+        quantity={quantity}
+        addToCart={addToCart}
+        removeFromCart={removeFromCart}
+      />
     </div>
   );
 }

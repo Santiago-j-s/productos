9a2b16621028dbd363cc311de7d05adb3bfcf5b5
Santiago Santana <santana.santiago@gmail.com>
2021-12-20 00:51:15 -0300

    refactor: remove remix demo files

--- app/root.tsx
+++ app/root.tsx
@@ -69,10 +69,6 @@ export function ErrorBoundary({ error }: { error: Error }) {
           <h1>There was an error</h1>
           <p>{error.message}</p>
           <hr />
-          <p>
-            Hey, developer, you should replace this with what you want your
-            users to see.
-          </p>
         </div>
       </Layout>
     </Document>
